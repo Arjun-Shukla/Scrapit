1b2b465a9f58aa2e0865a233f966e3401f5d47fd
Priyanshu_Kum@r <kumarpriyanshu30001@gmail.com>
2026-02-04 11:12:15 +0530

    all html/css implemented in django

--- DTU/scrapit/app/urls.py
+++ DTU/scrapit/app/urls.py
@@ -4,4 +4,7 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/rates', views.rates, name='rates'),
+    path('/userlogin', views.userlogin, name='userlogin'),
+    path('/usersignup', views.usersignup, name='usersignup')
 ]
--- DTU/scrapit/app/views.py
+++ DTU/scrapit/app/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def rates(request):
+    return render(request, 'rates.html')
+
+def userlogin(request):
+    return render(request, 'userlogin.html')
+
+def usersignup(request):
+    return render(request, 'usersignup.html')
